6bb72948b08e794d7bc5f7ec5ea6bf318bb04ad2
sheshu2906 <sheshnath.yadav@knoldus.com>
2023-03-15 11:53:38 +0530

    files updated

--- Session-1/Assignment-day-2/src/main/scala/FactorialTailRecursion.scala
+++ Session-1/Assignment-day-2/src/main/scala/FactorialTailRecursion.scala
@@ -2,14 +2,15 @@ import scala.annotation.tailrec
 import scala.io.StdIn
 
 object FactorialTailRecursion extends App{
-   val inputNumber = StdIn.readInt()
 
+    //get factorial method returns the factorial of the inputNumber
    @tailrec
    def getFactorial(inputNumber: Int, initialValue: Long): Long  = {
-       if (inputNumber <= 1) initialValue
+      if(inputNumber<0) throw new IllegalArgumentException()
+      else
+       if (inputNumber == 0) initialValue
        else
          getFactorial(inputNumber - 1, inputNumber * initialValue)
      }
-   println(getFactorial(inputNumber, 1))
  }
 
--- Session-1/Assignment-day-2/src/test/scala/FactorialTest.scala
+++ Session-1/Assignment-day-2/src/test/scala/FactorialTest.scala
@@ -1,10 +1,19 @@
 import org.scalatest.funsuite.AnyFunSuite
 
 class FactorialTest extends AnyFunSuite {
-  test("FactorialTailRecursion") {
+  test("Test case 1 if input number is 5 and initial value is 1") {
     assert(FactorialTailRecursion.getFactorial(5, 1) === 120)
-    assert(FactorialTailRecursion.getFactorial(1, 1) === 1)
-    assert(FactorialTailRecursion.getFactorial(34, 1) === 4926277576697053184L)
-
   }
+  test("Test case 2 if input number is 1 and initial value is 1") {
+    assert(FactorialTailRecursion.getFactorial(1, 1) === 1)
+  }
+  test("Test case 3 if input number is 0 and initial value is 1") {
+    assert(FactorialTailRecursion.getFactorial(0, 1) === 1)
+  }
+  test("Test case 4 if input number is -1 and initial value is 1") {
+    assertThrows[IllegalArgumentException] {
+      FactorialTailRecursion.getFactorial(-1,1)
+    }
+  }
+
 }
