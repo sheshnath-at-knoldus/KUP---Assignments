3bf66b24a92bb927495bcf8f51d88db095f6f1ad
sheshu2906 <sheshnath.yadav@knoldus.com>
2023-04-17 12:03:44 +0530

    Test Cases updated

--- Session-1/Assignment-day-1/src/test/scala/ElectricityBillCalculatorTest.scala
+++ Session-1/Assignment-day-1/src/test/scala/ElectricityBillCalculatorTest.scala
@@ -6,4 +6,20 @@ test("ElectricityCharges"){
   val caseClassReference = ElectricityBill(123,"sheshu","noida",5062,5904)
   assert(caseClassReference.calculateCharges(caseClassReference.currentUnit,caseClassReference.previousUnit)==7073.51)
 }
+
+  test("for small values") {
+    val caseClassReference = ElectricityBill(1, "shehnath yadav", "Gorakhpur", 50, 5904)
+    assert(caseClassReference.calculateCharges(caseClassReference.currentUnit, caseClassReference.previousUnit) == 57343.869999999995)
+  }
+
+  test("for negative value") {
+    val caseClassReference = ElectricityBill(1, "shehnath yadav", "Gorakhpur", 10000, -5904)
+    assert(caseClassReference.calculateCharges(caseClassReference.currentUnit, caseClassReference.previousUnit) == 0.0)
+  }
+
+
+  test("for zero value") {
+    val caseClassReference = ElectricityBill(1, "shehnath yadav", "Gorakhpur", 0, 5904)
+    assert(caseClassReference.calculateCharges(caseClassReference.currentUnit, caseClassReference.previousUnit) == 57845.369999999995)
+  }
 }
